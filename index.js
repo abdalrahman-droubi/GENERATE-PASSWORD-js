f26242da2352ebe49b1d4352c5724fde6a426b17
abdalrahman <abdalrahman@abdalrahman-droubi-pc.scoiumtech.local>
2023-09-03 15:25:03 +0300

    first push

---
+++ index.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("btn");
+const input = document.getElementById("inputPassword");
+const chars =
+  "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+passwordLength = 16;
+btn.addEventListener("click", () => {
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  input.value = password;
+});
